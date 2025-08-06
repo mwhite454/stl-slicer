ca2a58c57c41471a20d1bb08c159b536112c4e2d
Myka White <mwhite454@gmail.com>
2025-08-06 10:51:03 -0400

    Update SVG.tsx

--- src/app/compiler/SVG.tsx
+++ src/app/compiler/SVG.tsx
@@ -1,15 +1,21 @@
+import { Box } from '@mantine/core';
+
 export const SVG = ({svgContent, width, height, unit="mm", zoom=1}) => {
   return (
-    <div className="w-full h-full overflow-auto">
+    <Box style={{ width: '100%', height: '100%', overflow: 'auto' }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width={width + unit || '100%'}
         height={height + unit || '100%'}
-        style={{ transform: `scale(${zoom || 1})`, transformOrigin: '0 0' }}
+        style={{ 
+          transform: `scale(${zoom || 1})`, 
+          transformOrigin: '0 0',
+          width: '100%',
+          height: '100%'
+        }}
         viewBox={`0 0 ${width} ${height}`}
-        className="w-full h-full"
         dangerouslySetInnerHTML={{ __html: svgContent }}
       />
-    </div>
+    </Box>
   );
-}
+};
